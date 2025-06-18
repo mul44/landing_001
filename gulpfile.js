5cc75c520b42e7e83582eb27df314ac93065daf3
TheMul4001 <kochetilov066@gmail.com>
2025-06-18 15:57:11 +0300

    two init

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug')
+
+/* -----Static server----- */
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port:9000,
+            baseDir: "buid"
+        }
+    });
+
+    gulp.watch('build/**/*'),on('change', browserSync.reload);
+});
+
+
+/* -----Pug compile----- */
+gulp.task('template:compile', function buildHTML(){
+  return gulp.src('source/template/index.pug')
+    .pipe(pug({
+        pretty: true// Your options in here.
+      }))
+    
+});
